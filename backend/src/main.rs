b77831ab0e4263af4f81132f089dc53fcaee80c3
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2024-02-26 14:59:32 -0600

    _

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,6 @@
 use anyhow::Result;
 use clap::{Args, Parser, Subcommand};
-use futures_util::{SinkExt, StreamExt};
-use log::{debug, error};
+use log::error;
 use rand::Rng;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
@@ -12,11 +11,9 @@ use std::{
     ops::Range,
     path::PathBuf,
 };
-use warp::{
-    ws::{Message, WebSocket},
-    Filter,
-};
+use warp::Filter;
 
+mod connection;
 mod json_rpc;
 mod parser;
 
@@ -155,52 +152,11 @@ async fn server(args: ServerArgs) -> Result<()> {
     env_logger::init();
     let routes = warp::any().and(warp::ws()).map(|ws: warp::ws::Ws| {
         ws.on_upgrade(|ws| async move {
-            if let Err(e) = handle_ws(ws).await {
+            if let Err(e) = connection::handle_ws(ws).await {
                 error!("while handling websocket connection: {}", e);
             }
         })
     });
     warp::serve(routes).run(args.bind).await;
-    Ok(())
-}
-
-async fn handle_ws(ws: WebSocket) -> Result<()> {
-    use json_rpc::*;
-    let (mut tx, mut rx) = ws.split();
-    let mut last: Option<(QueryArgs, QueryResponse)> = None;
-    while let Some(Ok(msg)) = rx.next().await {
-        if let Ok(s) = msg.to_str() {
-            debug!("received: {}", s);
-            let new_query: JsonRpcQuery<QueryArgs> = serde_json::from_str(s)?;
-            // if query is substantially similar to the last one, use the cached response
-            if let Some((last_query, last_response)) = last.as_ref() {
-                if new_query.params.cols == last_query.cols
-                    && new_query.params.filter == last_query.filter
-                    && new_query.params.log_file == last_query.log_file
-                {
-                    let to =
-                        new_query.params.to.unwrap_or(last_response.total_display_lines);
-                    tx.send(Message::text(serde_json::to_string(&JsonRpcResponse {
-                        id: new_query.id,
-                        result: Some(last_response.range(new_query.params.from..to)),
-                        error: None,
-                    })?))
-                    .await?;
-                    continue;
-                }
-            }
-            let range_from = new_query.params.from;
-            let range_to = new_query.params.to;
-            let response = query(new_query.params.clone())?;
-            let range = range_from..range_to.unwrap_or(response.total_display_lines);
-            last = Some((new_query.params, response.clone()));
-            tx.send(Message::text(serde_json::to_string(&JsonRpcResponse {
-                id: new_query.id,
-                result: Some(response.range(range)),
-                error: None,
-            })?))
-            .await?;
-        }
-    }
     Ok(())
 }
