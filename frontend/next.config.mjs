1b6e776e929a0db5977114def6aa8ca80c507c3a
Andrew Lee <126016409+andrew-lee-architect@users.noreply.github.com>
2024-02-27 22:25:18 -0600

    configurable base path

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
+const basePath = process.env['BASE_PATH'];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: basePath ?? undefined,
+  assetPrefix: basePath ? basePath + '/' : undefined
 };
 
 export default nextConfig;
